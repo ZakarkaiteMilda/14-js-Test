101b579ed0d317bd571fac2f083dfef8a9028687
ZakarkaiteMilda <zakarkaite.milda@gmail.com>
2023-07-15 00:31:16 +1400

    Task 1

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 console.clear();
 console.log(`------------------------`);
+
+//1. Palyginti du skaičius a ir b. Išvesti į konsolę, kuris didesnis arba jie lygūs. (4 taškai)
+
+const number1 = 2;
+const number2 = 2;
+
+if (number1 == number2) {
+    console.log(`Skaičiai number1 ir number2 yra lygūs`);
+} else if (number1 >= number2) {
+    console.log(`Skaičius number1 yra didesnis už number2`);
+} else {
+    console.log(`Skaičius number1 yra mazesnis už number2`);
+}
+
